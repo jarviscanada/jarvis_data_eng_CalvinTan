b6e90040ec6a6afa5a6dfe41f173ef09519b9639
Calvin Tan <rocky@jarvis-remote-desktop.northamerica-northeast2-a.c.planar-outlook-426900-h4.internal>
2024-07-02 04:01:43 +0000

    implemented sql inserts

--- scripts/host_info.sh
+++ scripts/host_info.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+if [ "$#" -ne 5 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
 # save hostname and lscpu as a variable
 hostname=$(hostname -f)
 specs=`lscpu`
 
 # hardware info
+id=1
 hostname=$hostname
 cpu_number=$(echo "$specs" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$specs" | egrep "Architecture:" | awk '{print $2}' | xargs)
 cpu_model=$(echo "$specs" | egrep "Model name:" | awk '{$1=$2=""; print $0}' | xargs)
 cpu_ghz=$(echo "$specs" | egrep "Model name:" | awk '{print $NF}' | head -c -4 | xargs)
 cpu_mhz=$(echo $cpu_ghz | awk '{ printf("%.3f\n", $0*1000 ) }' | xargs)
-L2_cache=$(echo "$specs" | egrep "L2 cache:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$specs" | egrep "L2 cache:" | awk '{print $3}' | xargs)
 total_mem=$(awk '/MemTotal/ {print $2}' /proc/meminfo | xargs)
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+insert_stmt="INSERT INTO host_info(id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, timestamp, total_mem)
+  VALUES($id, '$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', $cpu_mhz, $l2_cache, '$timestamp', $total_mem);"
+
+export PGPASSWORD=$psql_password
+#Insert date into a database
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit $?
--- scripts/host_usage.sh
+++ scripts/host_usage.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+if [ "$#" -ne 5 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
 # save hostname and lscpu as a variable
 hostname=$(hostname -f)
 specs=`lscpu`
 
 #usage info
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 memory_free=$(vmstat --unit M | tail -1 | awk -v col="4" '{print $col}')
 cpu_idle=$(vmstat | tail -1 | awk -v col="15" '{print $col}')
 cpu_kernel=$(vmstat | tail -1 | awk -v col="14" '{print $col}')
 disk_io=$(vmstat --unit M -d | tail -1 | awk -v col="10" '{print $col}')
 disk_available=$(df -BM / | tail -1 | awk -v col="4" '{print $col}' | head -c -2 | xargs)
 
+export PGPASSWORD=$psql_password
+
+test_stmt="(SELECT id FROM host_info WHERE hostname='$hostname')"
+host_id=$(psql -t -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$test_stmt" | xargs)
+
+insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
+  VALUES('$timestamp', $host_id, $memory_free, $cpu_idle, $cpu_kernel, $disk_io, $disk_available);"
+
+#Insert date into a database
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit $?
