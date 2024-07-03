ce1b93d57b5eb38709a9df4ea60d8a81db87d398
Calvin Tan <rocky@jarvis-remote-desktop.northamerica-northeast2-a.c.planar-outlook-426900-h4.internal>
2024-07-03 15:42:11 +0000

    fixed typo

--- scripts/psql_docker.sh
+++ scripts/psql_docker.sh
@@ -9,13 +9,13 @@ db_password=$3
 sudo systemctl status docker > /dev/null || sudo systemctl start docker
 
 docker container inspect jrvs-psql > /dev/null
-contanier_status=$?
+container_status=$?
 
 # User switch case to handle create|stop|start operations
 case $cmd in
   create)
   # Check if container is alraedy created
-  if [[ "$container_status" -eq 0 ]]; then
+  if [ $container_status -eq 0 ]; then
     echo 'Container already exists'
     exit 1
   fi
@@ -26,12 +26,12 @@ case $cmd in
   fi
 
   docker volume create pgdata
-  docker run --name jrvs-psql -e POSTGRES_PASSWORD=$db_password -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
+  docker run --name jrvs-psql -e POSTGRES_USER="$db_username" -e POSTGRES_PASSWORD=$db_password -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
   ;;
 
   start|stop)
   # Check instance status
-  if [[ "$container_status" -ne 0 ]]; then
+  if [ "$container_status" -ne 0 ]; then
     echo 'Container does not exist'
     exit 1
   fi
