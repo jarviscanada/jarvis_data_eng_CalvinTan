1aec822e4bf2fdf2048d9c13ef54f82f2170195d
BasedBird <basedbird445@gmail.com>
2024-08-21 23:44:36 -0400

    fixed typo in class name

--- core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/dao/QuoteHttpHelper.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/dao/QuoteHttpHelper.java
@@ -3,25 +3,25 @@ package ca.jrvs.apps.stockquote.dao;
 import ca.jrvs.apps.stockquote.api.AlphaVantageAPI;
 import ca.jrvs.apps.stockquote.json.JsonParser;
 import ca.jrvs.apps.util.LoggerUtil;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import okhttp3.OkHttpClient;
 import okhttp3.Response;
 import org.slf4j.Logger;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Properties;
 
-public class QuotteHttpHelper {
+public class QuoteHttpHelper {
 
     private static final Logger logger = LoggerUtil.getLogger();
     private final OkHttpClient client;
     private final Properties properties;
     private final String apiKey;
 
-    public QuotteHttpHelper() {
+    public QuoteHttpHelper() {
         this.properties = new Properties();
         this.loadProperties();
         this.client = new OkHttpClient();
@@ -29,7 +29,7 @@ public class QuotteHttpHelper {
     }
 
     public static void main(String[] args) {
-        QuotteHttpHelper obj = new QuotteHttpHelper();
+        QuoteHttpHelper obj = new QuoteHttpHelper();
         try {
             Quote quote = obj.fetchQuoteInfo("MSFT");
             logger.debug(quote.getSymbol() + ": " + quote.getPrice());
@@ -45,9 +45,9 @@ public class QuotteHttpHelper {
      * @throws IllegalArgumentException no data was found for given symbol
      */
     public Quote fetchQuoteInfo(String symbol) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
         Response response = AlphaVantageAPI.get(this.client, symbol, this.apiKey);
         Quote quote = JsonParser.toObjectFromJson(response.body().string(), Quote.class);
+        quote.setTimestamp(Timestamp.valueOf(LocalDateTime.now()));
         return quote;
     }
 
